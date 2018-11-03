279d96ba0ce42cb924e593e26e7dd9dd86ef00f7
Deepali2 <hdgarg@gmail.com>
2018-11-02 17:29:50 -0700

    adds routes with parameters

--- app.js
+++ app.js
@@ -18,6 +18,21 @@ app.get("/dog", function(req, res) {
   res.send("Woof Woof");
 });
 
+app.get("/r/:subredditName", function(req, res) {
+  let subreddit = req.params.subredditName.toUpperCase();
+  res.send(`Welcome to a ${subreddit}`);
+});
+
+app.get("/r/:subredditName/comments/:id/:title/", function(req, res) {
+  console.log(req.params);
+  res.send("demomstrating ")
+})
+
+// get request to any url other than those defined. This must be after all the other routes
+app.get("*", function(req, res) {
+  res.send("You are a star!!!");
+});
+
 
 app.listen(3000, function() {
   console.log("Serving demo on port 3000");
