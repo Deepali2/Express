07bf2c269844414336d5e9dfa501c5db2731a89c
Deepali2 <hdgarg@gmail.com>
2018-11-03 07:39:32 -0700

    adds header footer styles to all the routes

--- 3Express/app.js
+++ 3Express/app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
-  res.render("home.ejs");
+  res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", function(req, res) {
-  let thing = req.params.thing;
-  res.render(`love.ejs`, {thingVar: thing});
+  let thing = req.params.thing;  
+  res.render("love", {thingVar: thing});
+});
+
+app.get("/posts", function(req, res) {
+  let posts =[
+    {title: "how to become a web developed", author: "Colt"},
+    {title: "Life", author: "Deepali"},
+    {title: "Be a good friend", author: "Harrry Potter"},
+  ];
+  res.render("posts", {posts: posts});
+});
+
+app.get("*", function(req, res) {
+  res.render("other");
 });
 
 app.listen(3000, function() {
-  console.log("I am listening on port 3000");
+  console.log("port 3000");
 });
